e3b3cbacad988730b9fb0bf7481b361ef4508104
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-25 14:25:06 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<main>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum in
+		quibusdam cum eius corporis architecto vel molestias perspiciatis unde
+		quaerat nobis facere totam nihil, quod provident eveniet possimus
+		blanditiis voluptatibus.!
+	</p>
+</main>
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
